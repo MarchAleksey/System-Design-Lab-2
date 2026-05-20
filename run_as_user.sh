fa543b678a8c6d95d3ba5bac1976d5fdd2b8da41
MarchAleksey <alex.marchenko5@mail.ru>
2026-05-21 00:00:36 +0300

    Revert "lab2"
    This reverts commit 954efaedaf8340446b7ae7a7332143cffd7b363e.

--- run_as_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -euo
-OLD_UID=$1
-OLD_GID=$2
-shift; shift
-groupadd --gid $OLD_GID --non-unique user 2>/dev/null || true
-useradd --uid $OLD_UID --gid $OLD_GID --non-unique user 2>/dev/null || true
-exec sudo -E -u user "$@"
--- src/handlers/handlers.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <userver/components/component_list.hpp>
-
-namespace messenger::handlers {
-
-void AppendMessengerHandlers(userver::components::ComponentList& component_list);
-
-}  // namespace messenger::handlers
